9f401e514a7a1efda6067eb31445a701f50a2abf
Alessandro Privé <alessandro.rossi7610@gmail.com>
2020-03-30 12:14:07 +0200

    Added a js file for the configurator script

---
+++ ProjetGamer/Scripts/configurator.js
@@ -0,0 +1,19 @@
+﻿
+function chooseItem(id) {
+    $('.selected-item').removeClass('selected-item');
+    $('.disabled').removeClass('disabled');
+
+    var selectedItem = document.getElementById("item" + id);
+    var selectedItemBtn = document.getElementById("btnItem" + id);
+    selectedItem.className += " selected-item";
+    selectedItemBtn.className += " disabled";
+    document.getElementById('hiddenChoosedItem').value = selectedItem.id;
+};
+
+function nextPage() {
+    var choosedItem = document.getElementById('hiddenChoosedItem').value;
+    $.post("CpuChoice", {
+        chooseItem: choosedItem
+
+    });
+}
