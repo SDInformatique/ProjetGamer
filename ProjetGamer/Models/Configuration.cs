2d1cf3805d915d4892cf6f76bf837db6392a3b15
Alessandro Privé <alessandro.rossi7610@gmail.com>
2020-03-30 12:16:33 +0200

    Added new model for the configuration, will be expanded later

---
+++ ProjetGamer/Models/Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetGamer.Models
+{
+    public class Configuration
+    {
+        /// <summary>
+        /// The ID(in DB) of the selected Processor
+        /// </summary>
+        private int cpuID;
+        /// <summary>
+        /// The ID(in DB) of the selected Graphics Card
+        /// </summary>
+        private int gpuID;
+        /// <summary>
+        /// The ID(in DB) of the selected Motherboard
+        /// </summary>
+        private int mbID;
+        /// <summary>
+        /// The ID(in DB) of the selected Case
+        /// </summary>
+        private int caseID;
+
+
+        public Configuration()
+        {
+            
+        }
+    }
+}
